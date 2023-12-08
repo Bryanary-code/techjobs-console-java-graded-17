4a4685a5d017c72a13df826d1e24b0dcaa666225
Bryan Wulfert <bwulfert@gmail.com>
2023-12-07 19:18:57 -0600

    assignment #1

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -75,9 +72,12 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
+        }
+        if (jobs.isEmpty()) {
+            System.out.print("No Results");
         }
 
         return jobs;
@@ -93,9 +93,20 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
         // TODO - implement this method
-        return null;
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> item : job.entrySet()) {
+                if (item.getValue().toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(job);
+                    }
+                }
+            }
+        if (jobs.isEmpty()) {
+            System.out.print("No Results");
+        }
+        return jobs;
     }
 
     /**
